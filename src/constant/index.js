201c7df5e5645d14d321c30f956de422996033c0
Abdirahman5863 <abdirahmanabdi5863@gmail.com>
2024-10-15 14:22:07 +0300

    final

--- src/constant/index.js
+++ src/constant/index.js
@@ -8,15 +8,15 @@ export const Projects = [
     
 "Step into the future of crowdfunding with my Web3 Kickstarter platform, fusing Solidity, TypeScript, Next.js, Tailwind CSS, and the third web's boundless potential. Join me in reshaping finance and projects with transparency and endless possibilities.",
     src: "/web3.png",
-    link:"https://web3kickstarter-lvhm.vercel.app/"
+    link:"https://web3kickstarter-s41l.vercel.app/"
   },
   
 { id:2,
-    title: "CheesCake",
+    title: "RateMyPLayer",
     text: 
-    "Embark on a cheesecake adventure with my meticulously crafted landing page powered by TypeScript, Next.js, and Tailwind CSS, navigating visually stunning designs for a taste of perfection in every click.",
-    src: "/cheescake.png",
-    link:"https://cheescake.vercel.app/"
+    "RateMyPlayer is a sports rating platform built with Next.js, MongoDB, Node.js, and Thirdweb. It allows users to rate and review players, coaches, and teams across various leagues, creating a community-driven space for sports enthusiasts. The platform features a robust authentication system and provides a seamless user experience for browsing, searching, and rating players..",
+    src: "/player.png",
+    link:"https://player-ratings.vercel.app/"
   },
     
 { id:3,
